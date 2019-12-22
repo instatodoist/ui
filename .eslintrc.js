00db549f768679fa643039b4c20b0a104aae4abe
techyaura <techyaura@gmail.com>
2019-12-22 23:17:40 +0530

    Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,17 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'max-line-length': [false],
+    'comma-dangle': 0,
+    "max-len": 0,
+    "no-underscore-dangle": 0,
+    "global-require": 0,
+    "indent": [
+      "error",
+      2
+    ],
+    "comma-dangle": 0,
+    "no-param-reassign": 0,
+    "no-prototype-builtins": 0
   },
   parserOptions: {
     parser: 'babel-eslint',
--- src/views/thought.vue
+++ src/views/thought.vue
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 <template>
   <v-container class="pa-2" fluid>
     <v-card color="basil">
@@ -7,14 +8,13 @@
       <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
         <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
       </v-tabs>
-      <div class="text-center" style="min-height: 500px;" v-if="$apollo.queries.listThought.loading">
-          <v-progress-circular
-            class="mt-10"
-            :size="50"
-            color="primary"
-            indeterminate
-          ></v-progress-circular>
-        </div>
+      <div
+        class="text-center"
+        style="min-height: 500px;"
+        v-if="$apollo.queries.listThought.loading"
+      >
+        <v-progress-circular class="mt-10" :size="50" color="primary" indeterminate></v-progress-circular>
+      </div>
       <!-- <div v-if="$apollo.queries.listThought.loading" class="ma-4"></div> -->
       <v-tabs-items
         v-model="tab"
@@ -90,70 +90,65 @@
     <v-btn bottom :color="getRandomColor" dark fab fixed right @click="dialog = !dialog">
       <v-icon>add</v-icon>
     </v-btn>
-    
+
     <v-dialog v-model="dialog" persistent max-width="600px">
-       <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-      <v-card>
-        <v-card-title>
-          <span class="headline">Next Desire to be fullfilled</span>
-        </v-card-title>
-        <v-card-text>
-          
-          <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field
-                  :rules="nameRules"
-                  v-model="thought.title"
-                  label="*What topic your mind pointing right now?"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-textarea
-                :rules="descriptionRules"
-                  v-model="thought.description"
-                  name="input-5-1"
-                  label="*Write in Detail"
-                  required
-                ></v-textarea>
-              </v-col>
-              <v-col>
-                <v-menu
-                  v-model="menu2"
-                  :close-on-content-click="false"
-                  :nudge-right="40"
-                  transition="scale-transition"
-                  offset-y
-                  full-width
-                  min-width="290px"
-                >
-                  <template v-slot:activator="{ on }">
-                    <v-text-field
-                      v-model="thought.accomplishTenure"
-                      label="Desire Accomplish Date (Optional)"
-                      prepend-icon="event"
-                      readonly
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker v-model="thought.accomplishTenure" @input="menu2 = false"></v-date-picker>
-                </v-menu>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <div class="flex-grow-1"></div>
-          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" text @click="addThought">Save</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-card>
+          <v-card-title>
+            <span class="headline">Next Desire to be fullfilled</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    :rules="nameRules"
+                    v-model="thought.title"
+                    label="*What topic your mind pointing right now?"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-textarea
+                    :rules="descriptionRules"
+                    v-model="thought.description"
+                    name="input-5-1"
+                    label="*Write in Detail"
+                    required
+                  ></v-textarea>
+                </v-col>
+                <v-col>
+                  <v-menu
+                    v-model="menu2"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="thought.accomplishTenure"
+                        label="Desire Accomplish Date (Optional)"
+                        prepend-icon="event"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="thought.accomplishTenure" @input="menu2 = false"></v-date-picker>
+                  </v-menu>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <div class="flex-grow-1"></div>
+            <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+            <v-btn color="blue darken-1" text @click="addThought">Save</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-form>
     </v-dialog>
 
@@ -189,37 +184,37 @@
   </div>-->
 </template>
 <script>
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 import {
   THOUGHT_QUERY,
   ADD_THOUGHT_MUTATION,
   UPDATE_THOUGHT_MUTATION
-} from "../gql/thought.gql";
+} from '../gql/thought.gql';
 
 export default {
-  name: "Thought",
+  name: 'Thought',
   data: () => ({
     dialogDelete: false,
     thoughtObj: null,
     loading: false,
     dialog: false,
     thought: {
-      title: "",
-      description: "",
+      title: '',
+      description: '',
       accomplishTenure: ''
     },
     nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 50) || 'Name must be less than 50 characters',
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 50) || 'Name must be less than 50 characters'
     ],
     descriptionRules: [
-        v => !!v || 'Description is required',
-        v => (v && v.length >= 50) || 'Name must be greater than 50 characters',
-      ],
+      v => !!v || 'Description is required',
+      v => (v && v.length >= 50) || 'Name must be greater than 50 characters'
+    ],
     tab: null,
-    items: ["Going-On", "Archieved"],
+    items: ['Going-On', 'Archieved'],
     menu2: false,
-    valid: true,
+    valid: true
   }),
   apollo: {
     listThought: {
@@ -231,17 +226,16 @@ export default {
       this.$apollo.queries.listThought.refetch();
     },
     async addThought() {
-       if (!this.$refs.form.validate()) {
-         return false;
-       }
+      if (!this.$refs.form.validate()) {
+        return false;
+      }
       const title = this.thought.title && this.thought.title.trim();
-      const description =
-        this.thought.description && this.thought.description.trim();
+      const description = this.thought.description && this.thought.description.trim();
       if (
-        !title &&
-        !description &&
-        typeof title === "string" &&
-        typeof description === "string"
+        !title
+        && !description
+        && typeof title === 'string'
+        && typeof description === 'string'
       ) {
         return;
       }
@@ -250,8 +244,11 @@ export default {
         title,
         description
       };
-      if(this.thought.accomplishTenure) {
-        postBody = {...postBody, accomplishTenure: this.thought.accomplishTenure}
+      if (this.thought.accomplishTenure) {
+        postBody = {
+          ...postBody,
+          accomplishTenure: this.thought.accomplishTenure
+        };
       }
       await this.$apollo.mutate({
         mutation: ADD_THOUGHT_MUTATION,
@@ -267,9 +264,8 @@ export default {
     },
     async markComplete(item) {
       let postBody = null;
-      const recordId =
-        typeof item !== "undefined" ? item._id : this.thoughtObj._id;
-      if (typeof item !== "undefined") {
+      const recordId = typeof item !== 'undefined' ? item._id : this.thoughtObj._id;
+      if (typeof item !== 'undefined') {
         postBody = {
           title: item.title,
           description: item.description,
@@ -311,35 +307,33 @@ export default {
     populateThought(thought) {
       this.thought = thought;
       console.log(thought);
-      this.thought.accomplishTenure = (thought.accomplishTenure) ? thought.accomplishTenure.toISOString().substr(0, 10) : '';
+      this.thought.accomplishTenure = thought.accomplishTenure
+        ? thought.accomplishTenure.toISOString().substr(0, 10)
+        : '';
     }
   },
   computed: {
     getRandomColor() {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
+      const letters = '0123456789ABCDEF';
+      let color = '#';
       for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
       }
       return color;
     },
     achieved() {
-      return this.listThought.data.filter(item => {
-        return item.isAchieved === true;
-      });
+      return this.listThought.data.filter(item => item.isAchieved === true);
     },
     notAchieved: {
       get() {
-        return this.listThought.data.filter(item => {
-          return item.isAchieved === false || item.isAchieved === null;
-        });
+        return this.listThought.data.filter(item => item.isAchieved === false || item.isAchieved === null);
       }
     }
   }
 };
 </script>
 <style scoped>
-  .isPinned{
-    background-color: rgb(76, 206, 43) !important;
-  }
+.isPinned {
+  background-color: rgb(76, 206, 43) !important;
+}
 </style>
