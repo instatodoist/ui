f9df32247a08efee5cfc655798bb4ddd90c062c6
techyaura <techyaura@gmail.com>
2020-05-06 01:19:26 +0530

    update version url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_URL: 'https://instatodo.herokuapp.com/graphql',
-  versionUrl: 'https://instant-todos.netlify.com/version.json'
+  versionUrl: 'https://instant-todos.netlify.app/version.json'
 };
