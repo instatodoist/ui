0db9976f80431fe3423b7fc7fd092166666f2a6c
techyaura <techyaura@gmail.com>
2020-05-06 09:04:51 +0530

    Fix homepage-slider

--- src/app/features/shared/layout/front/front.component.ts
+++ src/app/features/shared/layout/front/front.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterContentInit, AfterViewInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-front',
   templateUrl: './front.component.html',
   styleUrls: ['./front.component.scss']
 })
-export class FrontComponent implements OnInit {
+export class FrontComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    $('.owl-carousel').each(function() {
+      const jQuerycarousel = $(this);
+      jQuerycarousel.owlCarousel({
+          items: jQuerycarousel.data('items'),
+          loop: jQuerycarousel.data('loop'),
+          margin: jQuerycarousel.data('margin'),
+          nav: jQuerycarousel.data('nav'),
+          dots: jQuerycarousel.data('dots'),
+          autoplay: jQuerycarousel.data('autoplay'),
+          autoplayTimeout: jQuerycarousel.data('autoplay-timeout'),
+          navText: ['<i class="fa fa-angle-left fa-2x"></i>', '<i class="fa fa-angle-right fa-2x"></i>'],
+          responsiveClass: true,
+          responsive: {
+              // breakpoint from 0 up
+              0: {
+                  items: jQuerycarousel.data('items-mobile-sm'),
+                  nav: false,
+                  dots: true
+              },
+              // breakpoint from 480 up
+              480: {
+                  items: jQuerycarousel.data('items-mobile'),
+                  nav: false,
+                  dots: true
+              },
+              // breakpoint from 786 up
+              786: {
+                  items: jQuerycarousel.data('items-tab')
+              },
+              // breakpoint from 1023 up
+              1023: {
+                  items: jQuerycarousel.data('items-laptop')
+              },
+              1199: {
+                  items: jQuerycarousel.data('items')
+              }
+          }
+      });
+  });
   }
 
 }
