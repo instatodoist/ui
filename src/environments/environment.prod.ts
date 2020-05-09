8c2ae963dffc7326c7ab3877894384c01f215e8e
techyaura <techyaura@gmail.com>
2020-05-09 16:15:46 +0530

    Add prod images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_URL: 'https://instatodo.herokuapp.com/graphql',
-  versionUrl: 'https://instant-todos.netlify.app/version.json'
+  versionUrl: 'https://instatodos.netlify.app/version.json'
 };
