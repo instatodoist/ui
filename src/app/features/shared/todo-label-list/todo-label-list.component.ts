834ceca896cf0834d8b39f24d5ca698b70c5c966
techyaura <techyaura@gmail.com>
2020-05-30 19:03:06 +0530

    show number on sidebar tags

--- src/app/features/shared/todo-label-list/todo-label-list.component.ts
+++ src/app/features/shared/todo-label-list/todo-label-list.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../../../service/todo/todo.service';
 import { TodoLabelType, TodoLabelListType } from '../../../models/todo.model';
 import { Observable, from, of, forkJoin, interval } from 'rxjs';
-import { mergeMap, concat, toArray, map, scan, mergeAll, concatMap, concatAll, switchMap, flatMap, takeLast, reduce, take } from 'rxjs/operators';
+import { mergeMap, concat, toArray, map, scan, mergeAll, concatMap, concatAll, switchMap, flatMap, takeLast, reduce, take, elementAt } from 'rxjs/operators';
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 
 
 @Component({
@@ -32,23 +33,25 @@ export class TodoLabelListComponent implements OnInit {
   }
 
   getCountForLabels(labels: TodoLabelType[]): void {
-    const i = interval(2000).pipe(take(2));
-    i.subscribe(item => {
-      console.log(item, Math.random());
-    });
-    // from(labels)
-    //   .pipe(
-    //     mergeMap(item => this.todoService.listTodos(this.todoService.getConditions(item._id, 'labels'))),
-    //     map(item => item.totalCount || 0),
-    //     scan((acc, curr) => acc.concat(curr), []),
-    //   )
-    //   // forkJoin(it)
-    //   .subscribe(
-    //     (response: any) => {
-    //     console.log(response);
-    //     },
-    //     err => {
-    //       console.log(err);
-    //     });
+    from(labels)
+      .pipe(
+        concatMap(item => this.todoService.listTodosQuery(this.todoService.getConditions(item._id, 'labels'))),
+        map(item => item.totalCount || 0),
+        reduce((acc, curr) => {
+          return acc.concat(curr);
+        }, [])
+      )
+      // forkJoin(it)
+      .subscribe(
+        (response: any) => {
+          this.labels = this.labels.map((element, index) => {
+            element.count = response[index];
+            return element;
+          });
+          console.log(this.labels)
+        },
+        err => {
+          console.log(err);
+        });
   }
 }
