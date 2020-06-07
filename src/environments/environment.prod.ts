84e8e44a188138389798bb2f48c05a9d3a9364f9
techyaura <techyaura@gmail.com>
2020-06-07 23:02:59 +0530

    Add google analytics

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   API_URL: 'https://instatodo.herokuapp.com/graphql',
-  versionUrl: 'https://instatodos.netlify.app/version.json'
+  versionUrl: 'https://instatodos.netlify.app/version.json',
+  GTAG_ID: process.env.GTAG_ID
 };
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,17 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  // Google Analytics will be enabled on Production
+  // On deployment server Set the GTAG_ID
+  document.write(`
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DW9DEFJQ0S"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', ${environment.GTAG_ID});
+    </script>
+  `);
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
