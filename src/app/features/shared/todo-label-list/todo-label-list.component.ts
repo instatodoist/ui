560eb8a8ed14d875a166fa93bbd6370ac60c6d71
techyaura <techyaura@gmail.com>
2020-07-12 09:17:27 +0530

    Sidebar changes

--- src/app/features/shared/todo-label-list/todo-label-list.component.ts
+++ src/app/features/shared/todo-label-list/todo-label-list.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { TodoService } from '../../../service/todo/todo.service';
+import { TodoService, AppService } from '../../../service';
 import { TodoLabelType } from '../../../models/todo.model';
 
 @Component({
   selector: 'app-todo-label-list',
   templateUrl: './todo-label-list.component.html',
-  styleUrls: ['./todo-label-list.component.scss']
+  styles: [`
+    .child-labels{
+      background: var(--iq-light-primary) !important;
+    }
+  `]
 })
 export class TodoLabelListComponent implements OnInit {
 
   labels: TodoLabelType[];
+  currentUrl = '';
 
   constructor(
+    private appService: AppService,
     private todoService: TodoService
   ) { }
 
   ngOnInit(): void {
     this.getLabels();
+    this.appService.currentUrlObservable.subscribe( url => {
+      this.currentUrl = url;
+    });
   }
 
   getLabels() {
