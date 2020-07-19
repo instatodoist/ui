16bd4c4afe999979b77d84794de4d26081d955fb
techyaura <techyaura@gmail.com>
2020-07-19 14:45:01 +0530

    Add footer link for report issue

--- src/app/features/shared/section/footer/footer.component.ts
+++ src/app/features/shared/section/footer/footer.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IFooterLinks {
+  name: string;
+  href: string;
+  internal: boolean;
+}
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  links: IFooterLinks[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.links = [
+      // {
+      //   name: 'Privacy Policy',
+      //   href: '/pages/privacy-policy',
+      //   internal: true
+      // },
+      // {
+      //   name: 'Terms of Use',
+      //   href: '/pages/terms',
+      //   internal: true
+      // },
+      // {
+      //   name: 'Contact Us',
+      //   href: '/pages/contact-us',
+      //   internal: true
+      // },
+      {
+        name: 'Report an Issue',
+        href: 'https://github.com/sumanixinc/instatodos.com/issues/new',
+        internal: false
+      }
+    ];
   }
 
 }
